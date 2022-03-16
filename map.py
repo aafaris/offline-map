fa9756ebb244084ca6f5ab5ecebafd1abbb3ab1f
aafaris <faris-77777@hotmail.com>
2022-03-16 18:50:41 +0800

    gps update

--- map.py
+++ map.py
@@ -22,25 +22,20 @@ class Map(QMainWindow):
         self.setCentralWidget(self.graphWidget)
         self.setWindowTitle("Map Display")
 
-        self.verticalLayout = QtWidgets.QVBoxLayout(self)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.verticalLayout.addLayout(self.horizontalLayout)
 
-        self.lat_label = QLabel(self)
-        self.lat_label.setText("Enter Lat:")
-        self.lat = QLineEdit(self)
-        self.horizontalLayout.addWidget(self.lat_label)
-        self.horizontalLayout.addWidget(self.lat, alignment=QtCore.Qt.AlignCenter)
-        self.lon_label = QLabel(self)
-        self.lon_label.setText("Enter Lon:")
-        self.lon = QLineEdit(self)
-        self.horizontalLayout.addWidget(self.lon_label)
-        self.horizontalLayout.addWidget(self.lon, alignment=QtCore.Qt.AlignCenter)
+        self.latlon = QLineEdit(self)
+        self.latlon.setFixedWidth(270)
+        self.latlon.setPlaceholderText("1.34047, 103.70935 (latitude, longtitude)")
+        self.latlon.move(5,5)
+        self.horizontalLayout.addWidget(self.latlon, alignment=QtCore.Qt.AlignCenter)
 
         self.pb = QPushButton(self)
-        self.pb.setText("GPS")
-        self.pb.clicked.connect(lambda: self.getImageCluster(self.lat, self.lon))
-        # 1.304833, 103.831833
+        self.pb.setText("Update GPS")
+        self.pb.move(280, 5)
+        self.pb.clicked.connect(lambda: self.getImageCluster(self.latlon.text()))
         self.horizontalLayout.addWidget(self.pb)
 
         self.zoom_level = 15
@@ -48,8 +43,8 @@ class Map(QMainWindow):
         self.y_list = list()
         self.location_point = None
 
+        self.getImageCluster("1.34047, 103.70935")
         self.show()
-
 
     def deg2num_float(self, lat_deg, lon_deg, zoom):
         lat_rad = math.radians(lat_deg)
@@ -66,11 +61,23 @@ class Map(QMainWindow):
         ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
         return (xtile, ytile)
 
-    def getImageCluster(self, lat_deg, lon_deg, delta_lat=0.02,  delta_long=0.02):
-        if len(lat_deg) == 0 or len(lon_deg) == 0:
-            return 
+    def getImageCluster(self, latlon_deg, delta_lat=0.02,  delta_long=0.02):
 
-        smurl = 'tiles/{0}/{1}/{2}.png'
+        latlon_deg = latlon_deg.split(',')
+        lat_deg = latlon_deg[0].replace(" ", "")
+        lon_deg = latlon_deg[1].replace(" ", "")
+
+        self.x_list.clear()
+        self.y_list.clear()
+
+        try:
+            lat_deg = float(lat_deg)
+            lon_deg = float(lon_deg)
+        except ValueError:
+            print("Invalid longtitude and latitude!")
+            return
+
+        smurl = '{0}/{1}/{2}.png'
 
         self.xmin, self.ymax = self.deg2num(lat_deg - delta_lat, lon_deg - delta_long, self.zoom_level)
         self.xmax, self.ymin = self.deg2num(lat_deg + delta_lat, lon_deg + delta_long, self.zoom_level)
@@ -80,6 +87,10 @@ class Map(QMainWindow):
         for xtile in range(self.xmin, self.xmax+1):
             for ytile in range(self.ymin, self.ymax+1):
               imgurl = smurl.format(self.zoom_level, xtile, ytile)
+
+              if not os.path.exists(imgurl):
+                print("Invalid longtitude and latitude!")
+                return
 
               if xtile not in self.x_list:
                 self.x_list.append(xtile)
@@ -98,7 +109,9 @@ class Map(QMainWindow):
     def updateCoordinate(self, lat, lon):
         if self.location_point is not None:
             self.graphWidget.removeItem(self.location_point)
-    
+
+
+
         x, y = self.deg2num_float(lat, lon, self.zoom_level)
 
         x_pixel = (x % 1)*256
